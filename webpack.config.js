c93ff5062a6daaae5ecdba4b07f5bcbfb129741e
Ethan Fairweather <ebfairweather@gmail.com>
2017-02-24 13:08:19 -0500

    bootstraping app

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+ entry: './index.ts',
+ output: {
+   filename: 'bundle.js',
+   path: __dirname
+ },
+ module: {
+   rules: [
+     {
+       enforce: 'pre',
+       test: /\.js$/,
+       loader: "source-map-loader"
+     },
+     {
+       enforce: 'pre',
+       test: /\.tsx?$/,
+       use: "source-map-loader"
+     },
+     {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'less-loader'
+        ]
+      }
+   ]
+ },
+ resolve: {
+   extensions: [".tsx", ".ts", ".js"]
+ },
+ devtool: 'inline-source-map',
+};
