5be391851f175c869cce8f254500a07bcd1930c6
Ethan Fairweather <ebfairweather@gmail.com>
2017-03-07 09:43:08 -0500

    added app compoment

--- public/app.module.ts
+++ public/app.module.ts
@@ -1,1 +1,15 @@
 import {NgModule} from "@angular/core";
+
+import { AppComponent } from './app.component';
+
+@NgModule(
+    declarations:[
+        AppComponent
+    ],
+    imports:[
+
+    ],
+    providers[],
+    bootstrap: [AppComponent]
+
+)
