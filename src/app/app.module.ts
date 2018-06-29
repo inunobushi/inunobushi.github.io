644eb2857756ac642495d4a3c83829fbfb5f1467
inunobushi <rainuniverze@gmail.com>
2018-06-28 21:26:53 -0400

    pushing refactoring of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,44 @@
 import {NgModule} from "@angular/core";
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router'
+import { CollapseModule, TooltipModule } from 'ngx-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-@NgModule(
-    declarations:[
-        AppComponent
-    ],
-    imports:[
+import { Navbar} from './components/navbar/navbar.component';
+import { routes } from "./routes/routes.module";
 
+
+import { AuthService } from '../app/services/authservice/authentication.service';
+import { AuthGuard } from './common/authguard/authguard.guard';
+
+export function declarations(): any {
+    return [
+        Navbar
+    ]
+}
+
+
+@NgModule({
+    declarations: [ 
+        AppComponent,
+        declarations()
+    ],      
+    imports: [
+       BrowserModule,
+       RouterModule.forRoot(routes),
+       TooltipModule.forRoot(),
+       FormsModule,
+       HttpClientModule,
+       CollapseModule
     ],
-    providers[],
+    providers: [
+        AuthService,
+        AuthGuard
+    ],
     bootstrap: [AppComponent]
-
-)
+ })
+ export class AppModule { 
+   
+ }
